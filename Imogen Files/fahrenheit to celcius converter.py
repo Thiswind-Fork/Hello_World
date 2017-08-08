6a4220610b33b49ac86712540baf22b56a93f84e
ImogenLaurence <30843622+ImogenLaurence@users.noreply.github.com>
2017-08-08 21:23:29 +0100

    Revised Ch 6 Try It Out exercises
    Easygui: address formating and F to C conversion.

--- Imogen Files/fahrenheit to celcius converter.py
+++ Imogen Files/fahrenheit to celcius converter.py
@@ -1,6 +1,6 @@
 import easygui
 
 easygui.msgbox("This programme converts Fahrenheit to Celcius")
-fahrenheit = easygui.enterbox("Type in a temperature in Fahrenheit :")
+fahrenheit = float(easygui.enterbox("Type in a temperature in Fahrenheit :"))
 celcius = ( fahrenheit - 32 ) * 5.0 / 9
 easygui.msgbox("That is " + str(celcius) +" Degrees Celcius")
