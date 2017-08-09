33f17be7703943ade41a3d6b93623adb4388fde9
ImogenLaurence <30843622+ImogenLaurence@users.noreply.github.com>
2017-08-09 21:49:34 +0100

    Corrections to Problem Questions
    Three corrected files.

--- Imogen Files/chapter 8 try it out 1.py
+++ Imogen Files/chapter 8 try it out 1.py
@@ -1,3 +1,3 @@
-no = float(raw_input("Which multiplication table would you like?"))
+no = int(raw_input("Which multiplication table would you like?"))
 for i in range(1, 13):
     print no, "x", i, "=", no * i
--- Imogen Files/chapter 8 try it out 2.py
+++ Imogen Files/chapter 8 try it out 2.py
@@ -1,13 +1,10 @@
-no = float(raw_input("Which multiplication table would you like?(Type 3 to continue, anything else to quit)."))
-someInput = raw_input()
-while someInput == "3":
-    print "Thank you for the 3, very kind of you."
+no = int(raw_input("Which multiplication table would you like? "))
+x = 1
+print "Here's your table:"
+while x < 13:
     for i in range(1, 13):
-    print no, "x", i, "=", no * i
-    print "Type 3 to continue, anything else to quit."
-    someInput = raw_input()
-for i in range(1, 13):
-print no, "x", i, "=", no * i
-print "Thats not 3, so I'm quitting now."
+        print no, "x", i, "=", no * i
+        x = x + 1
+
 
 
