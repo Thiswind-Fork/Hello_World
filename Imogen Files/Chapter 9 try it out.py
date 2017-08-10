ed19fb20d690f219bf3df7fa118872b94a03109a
ImogenLaurence <30843622+ImogenLaurence@users.noreply.github.com>
2017-08-10 21:45:26 +0100

    Added Comments to Ch 9 TIO
    Added comments to Chapter 9 Try It Out exercise.

---
+++ Imogen Files/Chapter 9 try it out.py
@@ -0,0 +1,11 @@
+print "This programme converts Fahrenheit into celcius."
+# tells the programmer the purpose of this
+print "Type in the temperature in Fahrenheit: "
+fahrenheit = float(raw_input())
+# makes the answer = fahrenheit
+celcius = (fahrenheit - 32) * 5.0 / 9
+# works out the celcius equivilent
+print "That is",
+print celcius,
+print "Degrees Celcius"
+# tells the programmer what the answer is
